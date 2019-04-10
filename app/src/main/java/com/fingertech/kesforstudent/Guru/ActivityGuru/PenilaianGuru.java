c06d5422eaee41bcc41b578a28fc19f31e488817
fajaradiprast <fajaradiprast@gmail.com>
2019-04-10 15:30:54 +0700

    penilaian

---
+++ app/src/main/java/com/fingertech/kesforstudent/Guru/ActivityGuru/PenilaianGuru.java
@@ -0,0 +1,22 @@
+package com.fingertech.kesforstudent.Guru.ActivityGuru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.fingertech.kesforstudent.R;
+
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
+
+public class PenilaianGuru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.penilaian_guru);
+
+    }
+}
